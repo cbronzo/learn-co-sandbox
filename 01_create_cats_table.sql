f408077735a9082ea1aacab42c9963d75ea632f2
Catherine Bronzo <cbronzo@mac.com>
2021-02-07 04:18:53 +0000

    Automatically backed up by Learn

---
+++ 01_create_cats_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+
+INSERT INTO cats (name, age, breed) VALUES ('Lil Bub', 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, "Tabby");
